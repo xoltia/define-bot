83e1638b2e1dfb84420f511e0e107dfc2f380cba
xoltia <38849891+xoltia@users.noreply.github.com>
2019-08-15 16:15:25 -0500

    Actually does something now

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const Discord = require('discord.js');
-const config = require('./config.json')
+const fetch = require('node-fetch');
+const config = require('./config.json');
 
 const client = new Discord.Client();
+const API_URL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+const MERRIAM_BASE = "https://www.merriam-webster.com/dictionary/";
+
+function buildEmbed(word, data) {
+    const embed = new Discord.RichEmbed()
+        .setTitle(word)
+        .setURL(MERRIAM_BASE + word);
+    if (data.length == 0) {
+        embed.color = 0xff0000;
+        embed.description = "No definitions.";
+    }
+    for (let result of data) {
+        embed.addField(result.fl, result.shortdef.map((def, index) => `${index+1}. ${def}`).join('\n'));
+    }
+    return embed;
+}
 
 client.on('message', message => {
-
+    if (message.content.toLowerCase().startsWith('define')) {
+        const word = message.content.substr(6).trim().split(" ")[0];
+        if (word) {
+            fetch(API_URL + `${word}?key=${config.merriamWebsterKey}`)
+                .then(res => res.json())
+                .then(json => message.channel.send(buildEmbed(word, json)));
+        }
+    }
 });
 
 client.login(config.token);
